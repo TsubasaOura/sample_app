cc1387af363fed4f089ce7aef16220e63cde7e2a
TsubasaOura <tsubasaoura@OURAS-LAPTOP.localdomain>
2020-11-17 21:01:36 +0900

    tochu3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User,find_by(email: params[:session][:email].downcase)
-        if user $$ user.authenticate(params[:session][:passsword])
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:passsword])
             log_in user
             remember user
             redirect_to user
