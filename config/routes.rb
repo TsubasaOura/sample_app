9026ea7e32a117fa828b9756c183b7aa69e520c6
TsubasaOura <tsubasaoura@OURAS-LAPTOP.localdomain>
2020-12-06 18:41:28 +0900

    device change

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy' 
+  #get     '/users',   to: 'users#index'
   resources :users
 end
